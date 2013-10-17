153eb3d1f57c816c47d7911d6b52f1cf5c6d2ad8
Kryck <contact@anti-fraudes.com>
2013-10-17 18:41:21 +0200

    v2

--- _Database/base/world.sql
+++
@@ -1,1 +0,0 @@
--- OpenEMU World Database --
--- src/server/scripts/EasternKingdoms/BastionTwilight/boss_halfus.cpp
+++
@@ -1,7 +0,0 @@
-/*
-* Copyright (C) 2011-2013 OpenEmu <http://www.openemulator.com/>
-* 
-*/
-
-#include "ScriptPCH.h"
-#include "bastion_of_twilight.h"
